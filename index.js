74c15f610b76c8c12f91ee27a0d3ee49153d847c
Irfan-Julian <irfanjulian23@gmail.com>
2023-03-05 23:21:19 +0700

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(express.urlencoded({extended: true}))
 app.use(xss())
 app.use(cors({
-  origin: [`http://localhost:3000`, `https://recipppe.netlify.app/`],
+  origin: `https://recipppe.netlify.app/`,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
